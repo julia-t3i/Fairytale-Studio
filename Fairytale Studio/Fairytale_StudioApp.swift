da3b179e66f7214f4e751f52fd8252259df804d9
Julia <julia.t_01@icloud.com>
2026-06-08 16:03:20 -0400

    Initial Commit

---
+++ Fairytale Studio/Fairytale_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fairytale_StudioApp.swift
+//  Fairytale Studio
+//
+//  Created by Julia Teixeira on 2026-06-08.
+//
+
+import SwiftUI
+
+@main
+struct Fairytale_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
